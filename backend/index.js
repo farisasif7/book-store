90d329c63c28330e6901704bb86fd156625fcb57
farisasif7 <farisbinasif2016@gmail.com>
2023-12-25 18:32:53 +0500

    Handled CORS policy

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,25 @@
 import express from 'express';
 import { PORT, mongoDBURL } from './config.js';
 import mongoose from 'mongoose';
-import { Book } from './models/bookModel.js';
 import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
 
 const app = express();
 
 // Middleware for parsing request body
 app.use(express.json());
+
+// Middleware for handling CORS policy
+// Option 1: Allow all origins
+app.use(cors());
+
+// Option 2: Allow specific origins
+// app.use(cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type']
+// }));
+
 
 app.get('/', (request, response) => {
     console.log(request);
