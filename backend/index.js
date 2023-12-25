72073f0a1d0795c901b792ba8cfc30ac335e09db
farisasif7 <farisbinasif2016@gmail.com>
2023-12-25 11:22:49 +0500

    Added get a single book by id route

--- backend/index.js
+++ backend/index.js
@@ -55,6 +55,19 @@ app.get('/books', async (request, response) => {
     }
 });
 
+// Route to get a single book by id
+app.get('/books/:id', async (request, response) => {
+    try {
+        const { id } = request.params;
+        const book = await Book.findById(id);
+
+        return response.status(200).send({book});
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+});
+
 mongoose
     .connect(mongoDBURL)
     .then(() => {
